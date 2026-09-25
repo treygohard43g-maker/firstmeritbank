37683b0cf7997346ad25fff11dad17b3f88fa43a
treygohard43g-maker <treygohard43g@gmail.com>
2026-09-25 18:37:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ import { auth, db } from "./firebase.js";
 import {
   createUserWithEmailAndPassword,
   updateProfile
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 import {
   doc,
   setDoc,
   serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 
 const signupForm = document.getElementById("signupForm");
